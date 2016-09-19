8fda1334cbab96cd4806610d78be2f6e9ab4c3f3
shigemk2 <i.am.shige@gmail.com>
2016-09-19 14:39:03 +0900

    Add import scala.concurrent.ExecutionContext.Implicits.global

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -8,6 +8,8 @@ import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object Hello {
   def main(args: Array[String]): Unit = {
     val system =ActorSystem("mySystem")
