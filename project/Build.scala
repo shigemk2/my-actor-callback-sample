022762f0470b88ddb7127ec33d46f174322ffc9c
Michihito Shigemura <m_shigemura@shigemk2.com>
2017-01-22 17:23:36 +0900

    move to build.scala

---
+++ project/Build.scala
@@ -0,0 +1,21 @@
+import sbt._
+import Keys._
+
+object MyActorCallbackSampleProject extends Build {
+  import Resolvers._
+  lazy val root = Project("MyActorCallbackSample", file(".")) settings(coreSettings : _*)
+
+  lazy val commonSettings: Seq[Setting[_]] = Seq(
+    organization := "com.example",
+    version := "1.0",
+    scalaVersion := "2.11.8"
+  )
+
+  lazy val coreSettings = commonSettings ++ Seq(
+    name := """my-actor-callback-sample""",
+    libraryDependencies := Seq(
+      "com.typesafe.akka" %% "akka-actor" % "2.4.10",
+      "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+    )
+  )
+}
